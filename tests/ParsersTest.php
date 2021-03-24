c6284857978bccd1065eedbbe62d561f79d2dd48
yulia633 <ignat633yulia@yandex.ru>
2021-03-24 16:15:10 +0300

    added tests for parse function

--- tests/ParsersTest.php
+++ tests/ParsersTest.php
@@ -7,34 +7,50 @@ use PHPUnit\Framework\TestCase;
 use function Egg\Parsers\getString;
 use function Egg\Parsers\getNumber;
 use function Egg\Parsers\getWord;
+use function Egg\Parsers\parse;
 
 class ParsersTest extends TestCase
 {
     public function testGetString()
     {
-          $this->assertNull(getString(""));
-          $this->assertNull(getString("("));
-          $this->assertNull(getString("+(a, 10"));
-          $this->assertEquals(["\"()\"", "()"], getString("\"()\""));
+          $this->assertEquals("()", "()", getString("\"()\""));
     }
 
     public function testGetNumber()
     {
-          $this->assertNull(getNumber(""));
-          $this->assertNull(getNumber("+"));
-          $this->assertNull(getNumber("+(a, 10"));
-          $this->assertEquals(["9"], getNumber("9"));
-          $this->assertEquals(["9"], getNumber(9));
+          $this->assertEquals(0, getNumber("text"));
+          $this->assertEquals(1, getNumber("9"));
+          $this->assertEquals(1, getNumber(9));
     }
 
     public function testGetWord()
     {
-          $this->assertNull(getWord(" "));
-          $this->assertNull(getWord("("));
           $this->assertEquals(["+"], getWord("+(a, 10"));
           $this->assertEquals(["define"], getWord("define"));
           $this->assertEquals(["9"], getWord("9"));
           $this->assertEquals(["9"], getWord(9));
           $this->assertEquals(["pri"], getWord("pri nt"));
+    }
+
+    public function testParse()
+    {
+          $expected = [
+            'type' => 'apply',
+            'operator' => [
+                  'type' => 'word',
+                  'name' => '+'
+            ],
+            'args' => [
+                  [
+                        'type' => 'word',
+                        'name' => 'a'
+                  ],
+                  [
+                        'type' => 'value',
+                        'value' => '10'
+                  ]
+            ],
+          ];
+          $this->assertEquals($expected, parse("   +(a, 10)"));
     }
 }
